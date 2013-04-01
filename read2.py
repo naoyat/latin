c2d98fa739708d9af67edf0c4c08d625f4cf3f2b
naoya_t <naoya.tozuka@gmail.com>
2013-04-01 16:59:47 +0900

    read2リファクタ

--- read2.py
+++ read2.py
@@ -12,84 +12,70 @@ import ansi_color
 
 latin.latindic_load()
 
-def lookup(word, is_first=False):
+def lookup(word):
+    if ord(word[0]) <= 64: return None
+
     res = latin.latindic_lookup(word)
     if res: return res
 
-    if is_first and latin_char.isupper(word[0]):
-        l = latin_char.tolower(word)
-        res = latin.latindic_lookup(l)
+    if latin_char.isupper(word[0]):
+        word_lower = latin_char.tolower(word)
+        res = latin.latindic_lookup(word_lower)
         if res: return res
 
     if word[-3:] == u'que':
         res = latin.latindic_lookup(word[:-3])
         if res: return res
 
-    return None
+    return []
+
+
+def render_info(item):
+    name = {'present':'現在', 'imperfect':'未完了', 'perfect':'完了', 'future':'未来',
+            'active':'能動', 'passive':'受動',
+            'imperative':'命令', 'infinitive':'不定',
+            '-':'-'}
+    pos = item['pos']
+    if pos == 'noun':
+        return '%s %s' % (item['ja'], item.get('_', "-"))
+    elif pos == 'adj':
+        return 'a.%s %s' % (item['ja'], item.get('_', "-"))
+    elif pos == 'verb':
+        return 'v.%s %s%s %s.%s.%s' % (item['ja'],
+                                       item['person'], item['number'],
+                                       name[item.get('mood','-')],
+                                       name[item.get('voice','-')],
+                                       name[item.get('tense','-')],
+                                       )
+    elif pos == 'preposition':
+        return 'prep<%s> %s' % (item['dominates'], item['ja'])
+    else:
+        info = item.copy()
+        del info['surface'], info['pos'], info['ja']
+        if len(info) > 0:
+            return '%s %s %s' % (item['pos'], item['ja'], util.render(info))
+        else:
+            return '%s %s' % (item['pos'], item['ja'])
+
 
 def analyse(sentence):
     print ' '.join(sentence)
 
-    sentence_uc = []
-    maxlen = 0
-    for word in sentence:
-        word_uc = word.decode('utf-8')
-        maxlen = max(maxlen, len(word_uc))
-        sentence_uc.append(word_uc)
+    sentence_uc = [u.decode('utf-8') for u in sentence]
+    maxlen_uc = max(map(len, sentence_uc))
+    res = zip(sentence_uc, map(lookup, sentence_uc))
 
-#    def pp(info):
-#        pos = info['pos']
-#        if pos == 'n':
-#            cases_ja = [latin_noun.cases_ja[case[0:3]] + case[3:6] for case in info['case'].split('/')]
-#            return util.render([pos, info['base'], info['case'], info['gender'], info['ja'], cases_ja])
-#        else:
-#            return util.render(info)
-
-    first = True
-
-    l = len(sentence_uc)
-
-    def render_info(item):
-        name = {'present':'現在', 'imperfect':'未完了', 'perfect':'完了', 'future':'未来',
-                'active':'能動', 'passive':'受動',
-                'imperative':'命令', 'infinitive':'不定',
-                '-':'-'}
-        if item['pos'] == 'noun':
-            return '%s %s' % (item['ja'], item.get('_', "-"))
-        elif item['pos'] == 'adj':
-            return 'a.%s %s' % (item['ja'], item.get('_', "-"))
-        elif item['pos'] == 'verb':
-            return 'v.%s %s%s %s.%s.%s' % (item['ja'],
-                                           item['person'], item['number'],
-                                           name[item.get('mood','-')],
-                                           name[item.get('voice','-')],
-                                           name[item.get('tense','-')],
-                                           )
-        else:
-            info = item.copy()
-            del info['surface']
-            del info['pos']
-            del info['ja']
-            if len(info) > 0:
-                return '%s %s %s' % (item['pos'], item['ja'], util.render(info))
-            else:
-                return '%s %s' % (item['pos'], item['ja'])
-
-    for i in xrange(l):
-        word = sentence_uc[i]
-        print ansi_color.bold((u'    %*s' % (-maxlen, word)).encode('utf-8')),
-        # print u"    %*s" % (-maxlen, word),
-        c0 = ord(word[0])
-        if c0 > 64:
-            info = lookup(word, first)
-            first = False
-            if info:
-                print ' | '.join(map(render_info, info))
-            else:
-                print "(?)"
-        else:
+    for word, info in res:
+        print ansi_color.bold((u'  %*s ' % (-maxlen_uc, word)).encode('utf-8')),
+        if info is None:
             print
+        elif len(info) > 0:
+            print ' | '.join(map(render_info, info))
+        else:
+            print '(?)'
+
     print
+
 
 def main():
     if len(sys.argv) == 2:
