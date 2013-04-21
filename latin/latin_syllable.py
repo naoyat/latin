b20b5d97523f69b7887aa8d8da46f46f98e55b9f
naoya_t <naoya.tozuka@gmail.com>
2013-04-21 13:40:10 +0900

    音節分解とアクセント

---
+++ latin/latin_syllable.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+VOWEL = 0
+CONSONANT = 1
+
+def joint(chunk):
+    def cc(c1, c2):
+        if c1 in [u'p', u'b', u't', u'd', u'c', u'g', u'f'] and c2 in [u'l', u'r']:
+            return [c1+c2]
+        elif c1 == u's' and c2 in [u'p', u't', u'c']:
+            return [c1+c2]
+        else:
+            return [c1, c2]
+
+    chunk_len = len(chunk) - 1
+    if chunk_len == 1:
+        return chunk
+    elif chunk_len == 2:
+        if chunk[0] == VOWEL:
+            if chunk[1:] == [u'a', u'e']:
+                return [VOWEL, u'ae']
+            elif chunk[1:] == [u'a', u'u']:
+                return [VOWEL, u'au']
+            elif chunk[1:] == [u'o', u'e']:
+                return [VOWEL, u'oe']
+            else:
+                return chunk
+        # consonant
+        return [CONSONANT] + cc(chunk[1], chunk[2])
+    elif chunk_len == 3:
+        if chunk[0] == VOWEL: raise "Too long vowel chunk"
+        # consonant
+        return [CONSONANT, chunk[1]] + cc(chunk[2], chunk[3])
+    else:
+        raise "Too long chunk"
+
+
+def is_vowel(ch):
+    return ch in [u'a', u'e', u'i', u'o', u'u', u'y', u'ā', u'ē', u'ī', u'ō', u'ū', u'ȳ']
+
+
+def separate_syllaba(word_uc):
+    def type(ch):
+        return VOWEL if is_vowel(ch) else CONSONANT
+
+    last_type = None
+    chunks = []
+    for ch in word_uc:
+        curr_type = type(ch)
+        if last_type != curr_type:
+            chunks.append([curr_type])
+            last_type = curr_type
+        chunks[-1].append(ch)
+    # caesar: [1 c] [0 a e] [1 s] [0 a] [1 r]
+    # novembris: [1 n] [0 o] [1 v] [0 e] [1 m b r] [0 i] [1 s]
+    # deus: [1 d] [0 e u] [1 s]
+
+    chunks = map(joint, chunks)
+    # caesar: [1 c] [0 ae] [1 s] [0 a] [1 r]
+    # novembris: [1 n] [0 o] [1 v] [0 e] [1 m br] [0 i] [1 s]
+    # deus: [1 d] [0 e u] [1 s]
+
+    pre = []
+    vowel = []
+
+    res = []
+
+    for i, chunk in enumerate(chunks):
+        if chunk[0] == VOWEL:
+            vowel = chunk[1:]
+        else: # consonant
+            if len(chunk[1:]) == 1:
+                # 排出
+                if vowel != []:
+                    if len(vowel) == 2 and i == len(chunks)-1:
+                        res.append(pre + vowel[:1] + [])
+                        res.append(vowel[1:] + chunk[1:])
+                        pre = []
+                        vowel = []
+                    else:
+                        res.append(pre + vowel + [])
+                        pre = chunk[1:]
+                        vowel = []
+                else:
+                    pre = chunk[1:]
+            else:
+                res.append(pre + vowel + [chunk[1]])
+                pre = chunk[2:]
+                vowel = []
+
+    if vowel != []:
+        res.append(pre + vowel)
+    elif pre != []:
+        res[-1] += pre
+    # caesar: [c ae] [s a r]
+    # novembris: [n o] [v e m] [br i s]
+    # deus: [d e] [u s]
+
+    syl = [u''.join(r) for r in res]
+    # caesar: [cae sar]
+    # novembris: [no vem bris]
+    # deus: [de us]
+
+    return syl
+
+
+def locate_accent(syllables):
+    def is_long(syl):
+        if is_vowel(syl[-1]):
+            return syl[-1] in [u'ā', u'ē', u'ī', u'ō', u'ū', u'ȳ']
+        else:
+            return True
+    len_syl = len(syllables)
+    if len_syl == 1:
+        return None
+    elif len_syl == 2:
+        return 0
+    elif len_syl >= 3:
+        paenultima = len_syl-2
+        if is_long(syllables[paenultima]):
+            return paenultima
+        else:
+            return paenultima-1
---
+++ test/latin_syllable_test.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# A=ā E=ē I=ī O=ō U=ū Y=ȳ
+
+from latin.latin_syllable import separate_syllaba, locate_accent
+import unittest
+
+class SyllabaTestCase(unittest.TestCase):
+    def assertSep(self, word_utf8, expected_separation_utf8):
+        syllables = separate_syllaba(word_utf8.decode('utf-8'))
+        actual_separation_utf8 = (u'-'.join(syllables)).encode('utf-8')
+        self.assertEqual(actual_separation_utf8, expected_separation_utf8)
+
+    def assertAccent(self, word_utf8, expected_accent_utf8):
+        syllables = separate_syllaba(word_utf8.decode('utf-8'))
+        acc_idx = locate_accent(syllables)
+        if acc_idx is not None:
+            syllables[acc_idx] = u'[' + syllables[acc_idx] + u']'
+        actual_accent_utf8 = (u'-'.join(syllables)).encode('utf-8')
+        self.assertEqual(actual_accent_utf8, expected_accent_utf8)
+
+    def test_sep(self):
+        # v | Cv
+        self.assertSep("caesar", "cae-sar")
+        self.assertSep("rubicōne", "ru-bi-cō-ne")
+
+        # vC | Cv
+        self.assertSep("superātō", "su-pe-rā-tō")
+        self.assertSep("commīlitōnēs", "com-mī-li-tō-nēs")
+        # v | [pbtdcg][lr]v
+        self.assertSep("arīminēnsī", "a-rī-mi-nēn-sī")
+        self.assertSep("adlocūtus", "a-dlo-cū-tus") # can be "ad-lo-cū-tus"
+        self.assertSep("suggestum", "sug-ge-stum") # can be "sug-ges-tum"
+
+        # C | CC
+        self.assertSep("novembris", "no-vem-bris")
+
+        #
+        self.assertSep("lūx", "lūx")
+        self.assertSep("mer", "mer")
+        self.assertSep("vir", "vir")
+        self.assertSep("ex", "ex")
+        self.assertSep("in", "in")
+        self.assertSep("et", "et")
+        self.assertSep("sed", "sed")
+
+        self.assertSep("bellō", "bel-lō")
+        self.assertSep("dictātor", "dic-tā-tor")
+        self.assertSep("cōnsulēs", "cōn-su-lēs")
+
+        self.assertSep("deus", "de-us")
+        self.assertSep("suōs", "su-ōs")
+        self.assertSep("mēnsium", "mēn-si-um")
+
+    def test_accent(self):
+        # 単音節
+        self.assertAccent("lūx", "lūx")
+        self.assertAccent("mer", "mer")
+        self.assertAccent("vir", "vir")
+        self.assertAccent("ex", "ex")
+        self.assertAccent("in", "in")
+        self.assertAccent("et", "et")
+        self.assertAccent("sed", "sed")
+
+        # 2音節
+        self.assertAccent("caesar", "[cae]-sar")
+        self.assertAccent("bellō", "[bel]-lō")
+        self.assertAccent("suōs", "[su]-ōs")
+        self.assertAccent("forō", "[fo]-rō")
+
+        self.assertAccent("deus", "[de]-us")
+
+        # 3音節
+        ## long paenultima
+        self.assertAccent("dictātor", "dic-[tā]-tor")
+        self.assertAccent("rubicōne", "ru-bi-[cō]-ne")
+        self.assertAccent("commīlitōnēs", "com-mī-li-[tō]-nēs")
+
+        ## short paenultima
+        self.assertAccent("cōnsulēs", "[cōn]-su-lēs")
+        self.assertAccent("mēnsium", "[mēn]-si-um")
+
+
+if __name__ == '__main__':
+    unittest.main()
