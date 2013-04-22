e5f4434c4e6b8f0bc6607ceb4c01994d6fcab060
naoya_t <naoya.tozuka@gmail.com>
2013-04-22 15:10:43 +0900

    fero

--- latin/latin_verb_irreg.py
+++ latin/latin_verb_irreg.py
@@ -34,6 +34,63 @@ def conjugate_verb_eo(prefix=u'', ja=''):
     return items
 
 
+def conjugate_verb_fero(prefix=u'', ja=''):
+    tags = {'pos':'verb', 'ja':ja}
+
+    inf = prefix + u'ferre'
+    pres_stem = prefix + u'fer'
+    perf1sg = prefix + u'tulī'
+    pf_stem = perf1sg[:-1] #tul
+
+    supinum = prefix + u'lātum'
+
+    items = []
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'mood':'infinitive', 'tense':'present'}),
+                       [u'ferre'], [{}])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'mood':'infinitive', 'tense':'perfect'}),
+                       [u'tulisse'], [{}])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'mood':'infinitive', 'tense':'future'}),
+                       [u'lātūrus esse'], [{}])
+
+    # 能動態 現在・過去・未来
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'tense':'present'}),
+                       [u'ferō', u'fers', u'fert', u'ferimus', u'fertis', u'ferunt'])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'tense':'imperfect'}),
+                       [u'ferēbam', u'ferēbās', u'ferēbat', u'ferēbāmus', u'ferēbātis', u'ferēbant'])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'tense':'future'}),
+                       [u'feram', u'ferēs', u'feret', u'ferēmus', u'ferētis', u'ferent'])
+
+    # 能動態 完了
+    items += conjugate_perfect(perf1sg, tags)
+    items += conjugate_past_perfect(perf1sg, tags)
+    items += conjugate_future_perfect(perf1sg, tags)
+
+    # 受動態 現在・過去・未来
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'passive', 'tense':'present'}),
+                       [u'feror', u'ferris', u'fertur', u'ferimur', u'feriminī', u'feruntur'])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'passive', 'tense':'imperfect'}),
+                       [u'ferēbar', u'ferēbāris', u'ferēbātur', u'ferēbāmur', u'ferēbāminī', u'ferēbantur'])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'passive', 'tense':'future'}),
+                       [u'ferar', u'ferēris', u'ferētur', u'ferēmur', u'ferēminī', u'ferentur'])
+
+    # 命令形
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'tense':'present', 'mood':'imperative'}),
+                       [None, u'fer', None, None, u'ferte', None])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'active', 'tense':'future', 'mood':'imperative'}),
+                       [None, u'fertō', u'fertō', None, u'fertōte', u'feruntō'])
+
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'passive', 'tense':'present', 'mood':'imperative'}),
+                       [None, u'ferre', None, None, u'feriminī', None])
+    items += conjugate(prefix, util.aggregate_dicts(tags, {'voice':'passive', 'tense':'future', 'mood':'imperative'}),
+                       [None, u'fertor', u'fertor', None, None, u'feruntor'])
+
+    # 分詞
+    items += conjugate_participle(pres_stem, supinum, tags)
+
+#    print "(fero)", util.render(items)
+    return items
+
+
 def conjugate_verb_sum(prefix=u'', ja=''):
     tags = {'pos':'verb', 'ja':ja}
 
@@ -47,7 +104,7 @@ def conjugate_verb_sum(prefix=u'', ja=''):
                        [inf], [{}])
     items += conjugate(pf_stem, util.aggregate_dicts(tags, {'voice':'active', 'mood':'infinitive', 'tense':'perfect'}),
                        [u'isse'], [{}])
-    items += conjugate(pf_stem, util.aggregate_dicts(tags, {'voice':'active', 'mood':'infinitive', 'tense':'perfect'}),
+    items += conjugate(pf_stem, util.aggregate_dicts(tags, {'voice':'active', 'mood':'infinitive', 'tense':'future'}),
                        [u'tūrus esse'], [{}])
 
     # 能動態 現在・過去・未来
@@ -99,6 +156,15 @@ def conjugate_verb_eo_composites():
     return items
 
 
+def conjugate_verb_fero_composites():
+    items = []
+
+    items += conjugate_verb_fero(u'', '運ぶ,耐える')
+#    items += conjugate_verb_fero(u'red', '戻る,帰る')
+
+    return items
+
+
 def conjugate_verb_sum_composites():
     items = []
 
@@ -109,8 +175,8 @@ def conjugate_verb_sum_composites():
     items += conjugate_verb_sum(u'inter', '間にある,相違する')
     items += conjugate_verb_sum(u'ob', '妨げになる')
     items += conjugate_verb_sum(u'prae', '先頭に立つ')
-    items += conjugate_verb_sum(u'pos', 'できる') # possum potuI posse
-    items += conjugate_verb_sum(u'prō', '役に立つ') # prOsum prOfuI prOfutUrus prOdesse
+    items += conjugate_verb_sum(u'pos', 'できる') # possum potuī posse
+    items += conjugate_verb_sum(u'prō', '役に立つ') # prOsum prOfuī prOfutUrus prOdesse
 
     return items
 
@@ -119,14 +185,18 @@ def load():
     items = []
 
     items += conjugate_verb_sum_composites()
-    # items += conjugate_irregular_verb(u'edō', '食べる') # edO EdI Esum edere/Esse
-    # items += conjugate_irregular_verb(u'eō', '行く') # eO IvI/iI itum Ire
-    # items += conjugate_irregular_verb(u'ferō', '運ぶ') # ferO tulI lAtum ferre
-    # items += conjugate_irregular_verb(u'fīō', '成る,生ずる') # fIO factus sum, fierI
-    # items += conjugate_irregular_verb(u'volō', '欲する') # volO voluI velle
-    # items += conjugate_irregular_verb(u'mālō', 'むしろ〜を欲する') # mAlO mAluI mAlle
-    # items += conjugate_irregular_verb(u'nōlō', '欲しない') # nOlO nOluI nOlle
+    # items += conjugate_irregular_verb(u'edō', '食べる') # edO ēdī ēsum edere/ēsse
+
+    # items += conjugate_irregular_verb(u'eō', '行く') # eO īvī/iī itum īre
     items += conjugate_verb_eo_composites()
+
+    # items += conjugate_irregular_verb(u'ferō', '運ぶ') # ferO tulī lAtum ferre
+    items += conjugate_verb_fero_composites()
+
+    # items += conjugate_irregular_verb(u'fīō', '成る,生ずる') # fīO factus sum, fierī
+    # items += conjugate_irregular_verb(u'volō', '欲する') # volO voluī velle
+    # items += conjugate_irregular_verb(u'mālō', 'むしろ〜を欲する') # mAlO mAluī mAlle
+    # items += conjugate_irregular_verb(u'nōlō', '欲しない') # nOlO nOluī nOlle
 
     return items
 
